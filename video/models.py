bec1b62f1123cfc8f0cebc2ec8c0ac82dc8d01b7
Carlos Garcia <tigrecarlos1@gmail.com>
2020-06-14 14:20:20 -0500

    Creando el modelo

--- video/models.py
+++ video/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=70)
+    slug = models.SlugField(max_length=50)
+    created_at = models.DateTimeField(auto_now=True)
+
+def __str__(self):
+    return self.name
+
